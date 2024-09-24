b621ad43ddb8f501663670eba068c6173c14fec3
Atlas <mfoulks1@gmail.com>
2024-09-23 21:42:40 -0400

    Added workflow to build and push demo to github pages

--- demo/src/components/theme-provider.tsx
+++ demo/src/components/theme-provider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 export type Theme = "dark" | "light" | "system";
 
